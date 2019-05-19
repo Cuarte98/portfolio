87465e9984175205646be85d1b003ad5ddcca04e
Franco Cuarterolo <fcuarterolo@gmail.com>
2019-05-19 20:55:36 -0300

    Updates

---
+++ precache-manifest.0dfdf26f760581917bc26e2b70b49f82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae20e8cabdc94dd6847345ec9dfa154c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f44274ff301cfeb99cbb",
+    "url": "/portfolio/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f44274ff301cfeb99cbb",
+    "url": "/portfolio/static/js/main.4f0b8dd1.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
